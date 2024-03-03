5ea9eec800446a728d02f39afaa5c30a579934e2
Jiehoon Lee <jiehoonn@bu.edu>
2024-03-03 15:21:57 -0500

    Implemented session storage utilized chat history box

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
+import { useState, useEffect } from 'react';
 import ChatBox from './ChatBox';
+import ChatHistory from './components/ChatHistory';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1 className="chat-title">CHAT CHAT</h1>
+interface IMessage {
+  input: string;
+  response: string;
+}
 
-      <div className='chatbox'>
-        <ChatBox />
+function App() {
+  
+  const [messages, setMessages] = useState<IMessage[]>(() => {
+    const savedMessages = sessionStorage.getItem('chatMessages');
+    return savedMessages ? JSON.parse(savedMessages) : [];
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem('chatMessages', JSON.stringify(messages));
+  }, [messages]);
+
+  /*
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  */
+ 
+  const addMessage = (input: string, response: string) => {
+    setMessages((prevMessages) => [...prevMessages, { input, response }]);
+  };
+  
+
+  return (
+    <div className="app-container">
+      <h1 className="chat-title">CHAT CHAT</h1>
+      <div className='chat-layout'>
+        <ChatHistory messages={messages} />
+        <ChatBox onMessageSend={addMessage} />
       </div>
     </div> 
   );
 }
-
 
 export default App;
