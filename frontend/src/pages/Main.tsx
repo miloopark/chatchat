226de6a580e462ebc4bf6e7526f56948bf16a0ef
Jiehoon Lee <jiehoonn@bu.edu>
2024-04-20 15:57:18 -0400

    added research evidence to landing page

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Typewriter } from 'react-simple-typewriter';
 import landingbackdrop from '../assets/landing.svg';
+import sourcegraph1 from '../assets/sourcegraph1.png';
+import sourcegraph2 from '../assets/sourcegraph2.png';
 import Navbar from '../components/Navbar';
 import '../App.css';
 
@@ -33,6 +35,27 @@ const Main = () => {
                 <button onClick={() => navigate('/login')} className="get-started-button">
                     Get Started
                 </button>
+            </div>
+            <div className = "research-container">
+            <span style={{ textDecoration: 'underline' }}>1. Animated vs. Static Storybooks: </span>
+            <br />
+            Research indicates that animated illustrations in storybooks, specifically those incorporating motion, can significantly enhance children's visual processing compared to static illustrations. This suggests a potential for better story comprehension through directed attention to story-relevant parts of illustrations. <br />
+            <br />
+            <span style={{ textDecoration: 'underline' }}>2. Effect on Learners' Motivation: </span>
+            <br />
+            The presence of motion in illustrations was found to attract children's attention effectively, guiding them towards parts of the illustration critical for understanding the story. This mechanism may explain the observed improvement in story comprehension with animated books.
+            </div>
+            <div className = "research-source">
+                <img src={sourcegraph1} alt="Descriptive Text" style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
+                <p style={{ fontSize: 'small', textIndent: '10px', marginTop: '4px' }}>
+                Barut Tugtekin E, Dursun OO. Effect of animated and interactive video variations on learners' motivation in distance Education. 
+                Educ Inf Technol (Dordr). 2022;27(3):3247-3276. doi: 10.1007/s10639-021-10735-5. Epub 2021 Sep 16. PMID: 34548839; PMCID: PMC8444525.
+                </p>
+                <img src={sourcegraph2} alt="Descriptive Text" style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
+                <p style={{ fontSize: 'small', textIndent: '10px', marginTop: '4px' }}>
+                Takacs ZK, Bus AG. Benefits of Motion in Animated Storybooks for Children's Visual Attention and Story Comprehension. 
+                An Eye-Tracking Study. Front Psychol. 2016 Oct 13;7:1591. doi: 10.3389/fpsyg.2016.01591. PMID: 27790183; PMCID: PMC5062825.
+                </p>
             </div>
         </div>
     );
